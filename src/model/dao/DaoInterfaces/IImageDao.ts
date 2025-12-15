a52b1a89d14dffa6c80047231ace2dd453896228
mcran127 <112192289+mcran127@users.noreply.github.com>
2025-12-14 17:21:08 -0700

    Delete src directory

--- src/model/dao/DaoInterfaces/IImageDao.ts
+++
@@ -1,3 +0,0 @@
-export interface IImageDao {
-    putImage(imageBytesBase64: string, fileExtension: string): Promise<string>;
-}
--- src/model/dao/DaoInterfaces/IUserDao.ts
+++
@@ -1,7 +0,0 @@
-import { UserDto } from "tweeter-shared";
-
-export interface IUserDao {
-  getUser(alias: string): Promise<UserDto | null>;
-  validateUser(alias: string, password: string): Promise<boolean>;
-  createUser(user: UserDto, password: string): Promise<void>;
-}
